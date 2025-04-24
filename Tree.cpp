1f79f320fd9343d8031e31b4c1ff3d437a61ea48
yashwant kumar <iamyash422gmail.com>
2025-04-24 13:17:03 +0530

    Incomplete tree implementation

--- Tree.cpp
+++ Tree.cpp
@@ -1,10 +1,40 @@
 #include<bits//stdc++.h>
 using namespace std;
 
+class Node{
+    public:
+    int data;
+    Node* left;
+    Node* right;
+    Node(int x){
+        this->data = x;
+        this->left = nullptr;
+        this->right = nullptr;
+    }
+};
+
+void buildTree(Node* &root, int x){
+    if(root==nullptr){
+        root = new Node(x);
+        return;
+    }
+    queue<Node*>q;
+    q.push(root);
+
+   
+}
+
 int main(){
-    int a;
-    cin >> a;
-    cout << a;
-    cout << "Hey";
+    Node* root = new Node(-1);
+    Node* curr = root;
+    curr = nullptr;
+
+    int n;
+    while(n--){
+        int e;
+        cin >> e;
+        buildTree(curr, e);
+    }
+
     return 0;
 }
